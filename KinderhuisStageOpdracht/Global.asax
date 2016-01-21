1176ea1b9bec9e7606bd8275337a5c0d2ba6fdfa
Bram Baert <bram.baert@hotmail.com>
2016-01-21 14:21:11 +0100

    Initial Commit

---
+++ KinderhuisStageOpdracht/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KinderhuisStageOpdracht.MvcApplication" Language="C#" %>
---
+++ KinderhuisStageOpdracht/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KinderhuisStageOpdracht.Startup))]
+namespace KinderhuisStageOpdracht
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
